42a49c393d0e20b170c49f0a60a1046131610070
ctaboadaR <taboadarondon@hotmail.com>
2022-12-29 15:06:54 +0100

    cambios menores

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace MyDotnet
         {
             services.AddControllersWithViews();
             //comentario CT
+            //Nuevo comentario
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
