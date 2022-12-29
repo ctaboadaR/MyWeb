8fe074929e11a69d43e9411eb1d3abd6a0aa5b80
ctaboadaR <taboadarondon@hotmail.com>
2022-12-29 13:09:48 +0100

    comentario

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace MyDotnet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //comentario CT
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
